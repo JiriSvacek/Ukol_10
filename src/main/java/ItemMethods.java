d811887e0efa70cf7f4d8f25a05804073ed4693c
Jiri Svacek <svacek.jiri@email.cz>
2022-09-19 01:15:00 +0200

    Doplnění getteru a setteru

--- src/main/java/ItemMethods.java
+++ src/main/java/ItemMethods.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class ItemMethods implements GoodsMethods {
 
-    private Statement statement;
     private String url;
     private String user;
     private String password;
@@ -101,5 +100,29 @@ public class ItemMethods implements GoodsMethods {
                 result.getString("description"),
                 result.getInt("numberInStock"),
                 result.getBigDecimal("price"));
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
